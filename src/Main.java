611524f8512653a5988a7cab00fcde072433c3e7
Ovseiko <59171779+Ovseiko@users.noreply.github.com>
2019-12-23 18:39:42 +0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import static lib.Function.*;
+
+public class Main {
+    public static void main(String[] args) {
+       System.out.println(sort("tttt aa bbb jjjjj ccc"));
+       System.out.println(isPhone("+7(999)999-99-99"));
+    }
+}
